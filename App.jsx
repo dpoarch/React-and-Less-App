6159df350fac2a59ac34e87bf2f6395764aa3112
dpoarch <davidpoarch@outlook.com>
2017-09-28 17:56:52 +0800

    Initial File Upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+   render() {
+      return (
+          <div className="container">
+            <h1 className="background">Hello World!!!</h1>
+            <h2>A quick brown fox jumps over the lazy dog.</h2>
+            <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+         </div>
+      );
+   }
+}
+
+export default App;
